4562d13788b8320a9e3d89ba5303843158787dd2
Ivan Pilot <ivanpilot@gmail.com>
2017-07-23 17:16:58 +0200

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,20 @@
 // Code ThemedDecoration Component Here
+import React from 'react'
+
+class ThemedParty extends React.Component{
+  render(){
+    var children = React.Children.map(this.props.children, child => {
+      return (
+        <div className={this.props.theme}>{child}</div>
+      )
+    })
+
+    return (
+      <div>
+        {children}
+      </div>
+    );
+  }
+}
+
+export default ThemedParty
--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import ThemedParty from './components/ThemedDecorations';
 
 ReactDOM.render(
   <div>
-    <Invitation>Hey, you should totally come to this awesome party I'm throwing!</Invitation>
-    <ThemedParty theme="heaven">
-      <p>A decoration</p>
-      <p>Some other decoration</p>
-      <p>A last decoration</p>
-    </ThemedParty>
+    <Invitation>"Hey, you should totally come to this awesome party I'm throwing!"</Invitation>
+    // <ThemedParty theme="heaven">
+    //   <p>A decoration</p>
+    //   <p>Some other decoration</p>
+    //   <p>A last decoration</p>
+    // </ThemedParty>
   </div>,
   document.getElementById('main')
 );
